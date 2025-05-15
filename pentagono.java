f55ab0a721b95b6c9fc9dcb8ed54f291a8ce5035
kkaynara08 <queirozkaynara@gmail.com>
2025-05-15 15:37:42 -0300

    Pentagono

---
+++ pentagono.java
@@ -0,0 +1,40 @@
+package br.edu.formasgeometricas;
+
+public class pentagono {
+	
+	//Atributos
+	private double lado;
+	private double apotema;
+	private double area;
+	
+	
+	
+	//Construtor
+	public pentagono() {
+		lado = 0;
+		area = 0;
+	}
+		
+		
+	//Métodos
+	public void recebaLado(double lado) {
+			this.lado = lado; 
+	}
+	
+	public void calcApotema() {
+		apotema = lado / (2 * Math.tan(Math.PI / 5));
+	}
+	
+	public void calcArea() {
+		area = (5 * lado * apotema) / 2;
+	}
+	
+	public void mostrarArea() {
+		System.out.println("A área do Pentágono é: " + area);
+	}
+	
+	public double mostrarAreaComRetorno () {
+		System.out.println(area);
+		return area;
+	}
+}
